96b17bd821f392304a12f9a62eebd2929d5cd7a1
Antonio Cunanan <antonio.cunanan@gmail.com>
2018-07-23 16:42:43 -0500

    feat: Style home page.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {ActionSheetController, NavController} from 'ionic-angular';
 import {DataProvider} from "../../providers/data/data.provider";
 import {INewspaper} from "../../providers/data/data.interface";
 
@@ -12,15 +12,49 @@ export class HomePage {
   newspapers: INewspaper[];
 
   constructor(public navCtrl: NavController,
+              public actionSheetCtrl: ActionSheetController,
               public dataProvider: DataProvider) {
   }
 
   ionViewDidLoad() {
-    this.dataProvider.getNewspapers()
-      .then(newspapers => {
-        console.log(newspapers);
-        this.newspapers = newspapers
-      });
+  }
+
+  addNewspaper() {
+    const actionSheet = this.actionSheetCtrl.create({
+      title: 'Add Newspaper',
+      buttons: [
+        {
+          text: 'Browse',
+          handler: () => {
+            console.log('Browse clicked');
+          }
+        },{
+          text: 'Search',
+          handler: () => {
+            console.log('Search clicked');
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  browse() {
+
+  }
+
+  search() {
+
+  }
+
+  loadNewspapers() {
+    this.dataProvider.getNewspapers().then(newspapers => this.newspapers = newspapers);
   }
 
 }
